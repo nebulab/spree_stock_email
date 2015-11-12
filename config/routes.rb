1d6382c52c53665782e62ef5d0b4ced251dd5723
Paolo Tatone <paolo.tatone@gmail.com>
2015-11-12 13:25:29 +0100

    defines only index route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   resources :stock_emails, only: :create
   namespace :admin do
-    resources :stock_emails
+    resources :stock_emails, :only => [:index]
   end
 end
